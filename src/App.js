907dd147af38779b04ce7c4f5368d93a0ca5f637
Julia <jdaianova@gmail.com>
2023-09-25 09:58:59 +0200

    15 commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <HashRouter basename='/dentalestetik'>
+      <HashRouter>
         <Menu />
         <main className="App-main">
           <Routes>
